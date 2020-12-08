78da09e085d5a61bd0a74d89bde97d9a082986b1
Sergey Medintsev <medintsev@MacBook-Pro.local>
2020-12-08 08:54:39 +0300

    Created config file

--- cramming/main.py
+++ cramming/main.py
@@ -1,105 +1,5 @@
-
 from random import choice
-
-QUESTIONS = {
-    'prepositions': {
-        'at': [
-            'school', 'home', 'work', 'university', 'the airport', 'the station', 'a bus stop', 'a party', 'the door',
-            '6 o\'clock', 'half past two', '7.45',
-            'Christmas', 'Easter night', 'the weekend'
-        ],
-
-        'in': [
-            'France', 'Paris',
-            'the kitchen',
-            'February', 'June',
-            '(the) winter',
-            'a shop', 'a museum',
-            'a park', 'a garden', 'a car',
-            '2011',
-            'the morning', 'the afternoon', 'the evening'
-        ],
-
-        'on': [
-            'a bike', 'a bus', 'a train', 'a plane', 'a ship',
-            '1st March',
-            'Tuesday', 'New Year\'s Day', 'Valentine\'s Day',
-            'the floor', 'a table', 'a shelf', 'the balcony', 'the roof', 'the wall'
-        ]
-    },
-
-    'ed': {
-        'd': [
-            'turned', 'described', 'complained', 'remained', 'played', 'lived', 'died', 'entered',
-            'recognized', 'realized', 
-            'revised', 'proved', 'agreed',
-        ],
-
-        't': [
-            'asked', 'faked', 'watched', 'cooked', 'kicked', 'washed',
-            'impressed',
-            'watched'
-        ],
-
-        'id': [
-            'painted', 'coincided', 'concluded', 'exaggerated',
-            'invented',
-        ]
-
-    },
-
-    'tenses': {
-        'Present Simple':
-            ['regular actions', 'well-known facts'],
-        'Present Continuous':
-            ['actions in progress now', 'temporary actions'],
-        'Present Perfect':
-            ['actions in the past with result in the present', 'experience', 'just, already, yet'],
-        'Past Simple':
-            [
-                'actions completed in the past', 'yesterday, last month, 2 days ago',
-                'story', 'actions at a stated past time'
-            ],
-        'Present Perfect Continuous':
-            [
-                'actions which started in the past, continued to the present and are still in progress, or have just finished',
-                'visible consequences'
-            ]
-    },
-
-    'Continuous vs non-Continuous verbs': {
-        'Continues':
-            [
-                'write', 'run', 'move', 'climb', 'read', 'slip', 'ski',
-                'sleep', 'stand', 'try', 'dance', 'go', 'walk', 'listen',
-                'paint', 'eat',
-            ],
-        'non-Continuous':
-            [
-                'like', 'want', 'need', 'prefer',
-                'know', 'realise', 'suppose', 'mean', 'understand', 'believe', 'remember',
-                'belong', 'fit', 'contain', 'consist', 'seem',
-                'see', 'hear', 'consist of'
-            ],
-    },
-
-    'nouns: Countable/Uncountable': {
-        'Countable':
-            [
-                'fork', 'experiment', 'job (на двух работах)', 'mistake', 'manual', 'scientist', 'language', 'dentist',
-                'accident', 'frog', 'course', 'garden', 'bed', 'notebook', 'chair', 'bacteria',
-                'month', 'meal', 'bank'
-            ],
-        'Uncountable':
-            [
-                'reading', 'work (много работы)', 'information', 'knowledge', 'money', 'rice', 'electricity', 'sand',
-                'music', 'accommodation', 'advice', 'damage', 'behaviour', 'bread',
-                'furniture', 'luck', 'news', 'traffic', 'weather', 'safety', 'chaos', 'food'
-            ]
-    },
-}
-
-QUESTION_NUM_LIMIT = 100
+import config
 
 def get_answer(options):
     possible_answers = {}
@@ -131,19 +31,19 @@ def print_stat(right_answer_num, question_num):
 print("""
 You can type 'exit' if you want to stop cramming.
 Question limit is {}.
-""".format(QUESTION_NUM_LIMIT))
+""".format(config.QUESTION_NUM_LIMIT))
 print('Choose topic:')
 
-answer = get_answer(QUESTIONS.keys())
+answer = get_answer(config.QUESTIONS.keys())
 if answer == '__exit__':
     print('Bye!')
     quit()
-questions = QUESTIONS[answer]
+questions = config.QUESTIONS[answer]
 
 
 question_num = 0
 right_answer_num = 0
-for i in range(QUESTION_NUM_LIMIT):
+for i in range(config.QUESTION_NUM_LIMIT):
     correct_answer = choice(list(questions))
     question = choice(questions[correct_answer])
     
